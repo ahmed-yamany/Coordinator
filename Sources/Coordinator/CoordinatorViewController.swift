c9dacb749af03313e08ac725bec9da45c0a5e802
Ahmed Yamany <yamany2862016@gmail.com>
2023-04-27 16:18:29 +0200

    enhance coordinator pattern

--- Sources/Coordinator/CoordinatorViewController.swift
+++ Sources/Coordinator/CoordinatorViewController.swift
@@ -6,18 +6,18 @@
 //
 
 import UIKit
-
-public enum CoordinationTypes {
-    case navigation
-    case segue
-}
+import MakeConstraints
 
 open class CoordinatorViewController: UIViewController{
- 
+
+    public private(set) var presenterViewController: CoordinatorViewController? = nil
+    public private(set) weak var presentedView:  UIView? = nil
+    public var presentedViewAnchoredConstraints: AnchoredConstraints? = nil
+    
     // the coordinator is responsible for managing navigation between view controllers.
     public var coordinator: Coordinator?
     public var coordinationType: CoordinationTypes = .navigation
-
+    
     public var userInfo: [String: Any]? = nil
     
     // used to notify the coordinator when the view controller is popped from the navigation stack.
@@ -27,4 +27,80 @@ open class CoordinatorViewController: UIViewController{
             self.coordinator?.pop()
         }
     }
+}
+
+extension CoordinatorViewController {
+    /// This Method presents the given view controller on top of the current view controller.
+    @objc public func presentViewController(_ viewControllerToPresent: CoordinatorViewController, completion: @escaping () -> Void = {} ){
+        viewControllerToPresent.presenterViewController = self
+        
+        // Set the presentedView property to the view of the view controller being presented.
+        self.presentedView = viewControllerToPresent.view
+        
+        // Add the presentedView to the current view controller's view hierarchy
+        guard let presentedView = self.presentedView else { return }
+        self.view.addSubview(presentedView)
+        presentedView.frame = self.view.bounds
+
+        // Animate the presentation of the presentedView at 0 seconds.
+        UIView.animate(withDuration: 0, delay: 0) {
+            presentedView.makeConstraints(
+                                        bottomAnchor: self.view.bottomAnchor,
+                                        leadingAnchor: self.view.leadingAnchor,
+                                        trailingAnchor: self.view.trailingAnchor)
+
+            presentedView.frame = .zero
+        }completion: { _ in
+            self.beginFullScreenAnimation(completion: completion)
+        }
+        
+    }
+    
+    private func beginFullScreenAnimation(completion: @escaping () -> Void) {
+        guard let presentedView = self.presentedView else { return }
+
+
+        UIView.animate(withDuration: 0.7,
+                       delay: 0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0.7,
+                       options: .curveEaseOut) {
+
+            self.navigationController?.navigationBar.isHidden = true
+            self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+
+            presentedView.fillXSuperViewConstraints()
+            self.presentedViewAnchoredConstraints = presentedView.makeConstraints(topAnchor: self.view.topAnchor, bottomAnchor: self.view.bottomAnchor)
+            
+            self.view.layoutIfNeeded()
+            presentedView.layoutIfNeeded()
+        } completion: { _ in
+            completion()
+        }
+    }
+    
+    @objc public func dismiss(completion: @escaping () -> Void = {}) {
+        self.presenterViewController?.navigationController?.navigationBar.isHidden = false
+        self.presenterViewController?.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        
+        UIView.animate(withDuration: 0.7,
+                       delay: 0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0.7,
+                       options: .curveLinear) {
+            
+            self.presenterViewController?.presentedViewAnchoredConstraints?.top?.constant = self.view.height
+            
+            self.presenterViewController?.view.layoutIfNeeded()
+            self.presenterViewController?.presentedView?.layoutIfNeeded()
+            
+        } completion: { _ in
+            self.presenterViewController?.presentedView?.removeFromSuperview()
+            self.presenterViewController?.presentedView = nil
+            completion()
+        }
+    }
+    
+
+
 }
